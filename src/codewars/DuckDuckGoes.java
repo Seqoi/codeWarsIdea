435aaa24efae3baaac00ef32003775c7466dfdb2
sd <ddarikin123497@gmail.com>
2023-12-13 10:11:40 +0300

    second kata : error wile working with objects "DUCKDUCKGOES"

--- src/codewars/DuckDuckGoes.java
+++ src/codewars/DuckDuckGoes.java
@@ -1,4 +1,14 @@
 package codewars;
 
 public class DuckDuckGoes {
+    public static Player duckDuckGoose(Player[] players, int goose) {
+        Player duckDuckCome = "";
+        while (goose > players.length){
+            goose = goose - players.length;
+        }
+             duckDuckCome = players[(goose - 1)];
+
+        return duckDuckCome;
+    }
+
 }
