3a2a1c82951e6c55e18b0de00521101687ba877f
sd <ddarikin123497@gmail.com>
2023-12-27 19:02:48 +0300

    third kata : start

---
+++ src/codewars/RemoveExclamation.java
@@ -0,0 +1,14 @@
+package codewars;
+
+public class RemoveExclamation {
+    public static String remove(String s, int n){
+        while (n>0){
+            char[] = s.toCharArray();
+            while(char[].hasNext
+            char[]
+            s = s.replace("!","");
+            n = n - 1;
+        }
+        return s;
+    }
+}
