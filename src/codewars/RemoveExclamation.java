52e6c954ae7b5b6f2a2715ce65c8e3fd02f51e4f
sd <ddarikin123497@gmail.com>
2023-12-28 16:36:36 +0300

    third kata : midle

--- src/codewars/RemoveExclamation.java
+++ src/codewars/RemoveExclamation.java
@@ -2,12 +2,17 @@ package codewars;
 
 public class RemoveExclamation {
     public static String remove(String s, int n){
+        char[] str;
+        str = s.toCharArray();
+        char c1 = '!';
         while (n>0){
-            char[] = s.toCharArray();
-            while(char[].hasNext
-            char[]
-            s = s.replace("!","");
-            n = n - 1;
+          for (int i = 0; i< str.length; i++){
+              if (c1 == str[i]){
+                  str = ArrayUtils.removeElement(str, i+1);
+              }
+              continue;
+          }
+          n = n - 1;
         }
         return s;
     }
